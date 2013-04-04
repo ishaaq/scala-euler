00eabe1829d3a6491452cf4665e2e6de2329671b
Ishaaq Chandy <ishaaq@gmail.com>
2013-04-04 21:55:00 +1100

    answer 4

--- src/main/scala/scalaeuler/Prob001to010.scala
+++ src/main/scala/scalaeuler/Prob001to010.scala
@@ -45,7 +45,17 @@ object Prob001to010 {
    * A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 99.
    * Find the largest palindrome made from the product of two 3-digit numbers.
    */
-  def prob004: Long = ???
+  def prob004: Long = {
+    def isPalindrome(num: Long) = {
+      val str = num.toString
+      str.reverse == str
+    }
+
+    val palindromeNumbers = for ( x <- 100 to 999;
+                                  y <- 100 to 999;
+                                  prod = x * y if isPalindrome(prod)) yield prod
+    palindromeNumbers.max
+  }
 
   /**
    * Smallest multiple
