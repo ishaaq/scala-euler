459595919c9ca5ead20db2c18479d4e8e48626a2
Ishaaq Chandy <ishaaq@gmail.com>
2013-04-05 12:58:41 +1100

    Problems 11 to 20

---
+++ src/test/scala/scalaeuler/Prob011to020Test.scala
@@ -0,0 +1,17 @@
+package scalaeuler
+
+import Prob011to020._
+import org.scalatest.FlatSpec
+
+class Prob011to020Test extends FlatSpec {
+  "The answer to prob011: " should "be 70600674" in assert(prob011 === 70600674L)
+  "The answer to prob012: " should "be 76576500" in assert(prob012 === 76576500L)
+  "The answer to prob013: " should "be 5537376230" in assert(prob013 === 5537376230L)
+  "The answer to prob014: " should "be 837799" in assert(prob014 === 837799L)
+  "The answer to prob015: " should "be 137846528820" in assert(prob015 === 137846528820L)
+  "The answer to prob016: " should "be 1366" in assert(prob016 === 1366L)
+  "The answer to prob017: " should "be 21124" in assert(prob017 === 21124L)
+  "The answer to prob018: " should "be 1074" in assert(prob018 === 1074L)
+  "The answer to prob019: " should "be 171" in assert(prob019 === 171L)
+  "The answer to prob020: " should "be 648" in assert(prob020 === 648L)
+}
